05f165e5e84ddd99fa916e6f8b5d85e4f1fe8495
SpikeNew7774 <88393630+SpikeNew7774@users.noreply.github.com>
2023-03-19 16:21:26 +0100

    Add files via upload

---
+++ bl-sc-img-mainpage.js
@@ -0,0 +1,36 @@
+ // GGG SPEDUP IMG
+  fetch('./ggg-spedup/ggg-spedup.png')
+  .then(res => res.blob())
+  .then(res => generateIMG1(res))
+
+function generateIMG1(blob) {
+
+  const urlBlIMG1 = URL.createObjectURL(blob);
+	document.querySelector("#ggg-spedup img").src = urlBlIMG1
+
+};
+
+
+ // BH - JS SPEDUP IMG
+ fetch('./babyhotline-spedup/thumbnail.webp')
+ .then(res => res.blob())
+ .then(res => generateIMG2(res))
+
+function generateIMG2(blob) {
+
+ const urlBlIMG2 = URL.createObjectURL(blob);
+   document.querySelector("#bh-spedup img").src = urlBlIMG2
+
+};
+
+ // GD RETRAY - 1 ATTEMPT IMG
+ fetch('./gd-retray-1attempt/thumbnail.webp')
+ .then(res => res.blob())
+ .then(res => generateIMG3(res))
+
+function generateIMG3(blob) {
+
+ const urlBlIMG3 = URL.createObjectURL(blob);
+   document.querySelector("#gd-retray-1attempt img").src = urlBlIMG3
+
+};
