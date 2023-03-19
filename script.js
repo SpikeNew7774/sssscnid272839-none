b431d41ceb6c0232982ecd086ba31c2c985dc401
SpikeNew7774 <88393630+SpikeNew7774@users.noreply.github.com>
2023-03-19 16:50:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+var gggspeduplbtn = document.querySelector("#ggg-spedup #lbtn")
+var gggspedup = document.querySelector("#ggg-spedup")
+var watchavid = document.querySelector("#watchavid")
+
+var bhspeduplbtn = document.querySelector("#bh-spedup #lbtn")
+var bhspedup = document.querySelector("#bh-spedup")
+
+var gdretray1attbtn = document.querySelector("#gd-retray-1attempt #lbtn")
+var gdretray1att = document.querySelector("#gd-retray-1attempt")
+
+gggspeduplbtn.onclick = function gggspup() {
+    $('#watchavid').load('./ggg-spedup/ggg-spedup.html');
+    gggspedup.style.display = "none"
+    bhspedup.style.display = "none"
+    gdretray1att.style.display = "none"
+    watchavid.style.display = "block"
+    document.getElementById("tit").style.display = "none"
+    document.title = "Spikerko - Streamovací Servis - Green Green Grass - SpedUp - SpikeNew7774"
+}
+
+bhspeduplbtn.onclick = function bhspup() {
+    $('#watchavid').load('./babyhotline-spedup/bh-spedup.html');
+    gggspedup.style.display = "none"
+    bhspedup.style.display = "none"
+    gdretray1att.style.display = "none"
+    watchavid.style.display = "block"
+    document.getElementById("tit").style.display = "none"
+    document.title = "Spikerko - Streamovací Servis - Baby Hotline - SpedUp - SpikeNew7774"
+}
+
+
+
+gdretray1attbtn.onclick = function bhspup() {
+  $('#watchavid').load('./gd-retray-1attempt/gd.html');
+  gggspedup.style.display = "none"
+  bhspedup.style.display = "none"
+  gdretray1att.style.display = "none"
+  watchavid.style.display = "block"
+  document.getElementById("tit").style.display = "none"
+  document.title = "Spikerko - Streamovací Servis - Geometry Dash - ReTraY - 1 Attempt"
+}
+
+
+const img = document.querySelector("img");
+img.ondragstart = () => {
+  return false;
+};
+img.setAttribute("draggable", false);
