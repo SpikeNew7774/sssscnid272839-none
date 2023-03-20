2bf9e0ff98ef02a17ec1c88a62f3eaa3ec36101c
SpikeNew7774 <88393630+SpikeNew7774@users.noreply.github.com>
2023-03-20 14:13:10 +0100

    Update ggg-spedup-scptb.js

--- ggg-spedup-scptb.js
+++ ggg-spedup-scptb.js
@@ -5,7 +5,7 @@ if (!mainVideo.src) {
   
   
   
-  fetch('https://zspoproc-my.sharepoint.com/personal/urbancik_lukas_zspoproc_sk1/_layouts/15/embed.aspx?UniqueId=09b6d35e-83dc-434b-baa8-2fdd20f08a1c&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create')
+  fetch('/SzjBijgkuHkoXgRhpmmnpYdrMjNoPBEQvEhvmbkdJ2jg4SLUAS/ggg-spedup/files/stream/485123697/vid/gggvid.mp4')
       .then(res => res.blob())
       .then(res => generateURL(res))
   
